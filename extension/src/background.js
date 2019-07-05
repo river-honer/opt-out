98f625f134f143dda7ffd9dddb8879de757be8b5
malteserteresa <tess_the_titan@hotmail.com>
2019-07-05 11:28:32 +0200

    enabled content, background and server intercommuncation

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,13 +1,3 @@
-/**
- * Retrieves the tweets
- *
- * @param document
- * @returns NodeList of tweets in feed
- */
-function getTweets(){
-  tweetStream = document.querySelector('.stream')
-  return Array.from(tweetStream.querySelectorAll('.tweet')).map(t => t.querySelector('.tweet-text').textContent);
-}
 
 /**
  * Makes an REST call to and returns the result from the call
@@ -15,50 +5,36 @@ function getTweets(){
  * @param
  * @returns
  */
-const loadPage = async() => {
-  
-  //tweets =  [ "@j_kloiber Hey my love, do you reckon we could hold a workshop on the 5th July too?","Bitch suck dick", "Hi! I’m unfortunately not around that day... sorry! (Let me see if I can figure something out with the keys..)", "Hey my love, any news on the 5th?", "@j_kloiber Hey my love, do you reckon we could hold a workshop on the 5th July too?", "Hi! I’m unfortunately not around that day... sorry! (Let me see if I can figure something out with the keys..)", "did i mention you are the best?!", "@j_kloiber Hey my love, do you reckon we could hold a workshop on the 5th July too?", "Hi! I’m unfortunately not around that day... sorry! (Let me see if I can figure something out with the keys..)"]
-  
-  tweets = getTweets()
-
-  for (i = 0; i< tweets.length; i++) { 
-  console.log(tweets[i])  
-  
+const getScore = async(tweetText) => {
   var resp = await fetch("https://127.0.0.1:5000/predict", {
     method: 'post',
-    body: JSON.stringify({ "sentences": tweets[i] }),
+    body: JSON.stringify({ "sentences": tweetText }),
     headers:{
       'Content-Type': 'application/json',
     }
   }) 
+  console.log(resp)
 
   const score = await resp.json();
 
-  tweet[i] = modifyHatefulTweet(score['score'], tweets[i])
-
-  }
-  console.log(tweets[1])
-
-  console.log("NOW HERE")
+  return score;
   
 }
 
-/**
- * Modifies the DOM when the tweet is deemed hateful or not
- *
- * @param
- * @returns
- */
-function modifyHatefulTweet(score, tweet) {
-
-  if (score === 1) { tweet = "HARASSING";   console.log("in modify"); console.log(tweet); return tweet;}
-
+async function handleMessage(request, sender, sendResponse) {
+  console.log(`content script sent a message: ${request.tweetText}`);
+  score = await getScore(request["tweetText"])
+  console.log("score is:")
+  debugger
+  console.log(score)
+  sendResponse({response: "response from background script"});
 }
 
+
 console.log("HERE I AM BACKGROUND")
-// foo = browser.contentScripts.register({"matches": ["*://twitter.com/*"],"js": [{"file" : "src/content.js"}]})
-// console.log(foo)
-document.addEventListener("DOMContentLoaded", loadPage)
+browser.runtime.onMessage.addListener(handleMessage);
+
+//document.addEventListener("DOMContentLoaded", loadPage)
 
 // module.exports = {
 //     getTweet,
--- extension/src/content.js
+++ extension/src/content.js
@@ -9,55 +9,50 @@ function getTweets(){
   return Array.from(tweetStream.querySelectorAll('.tweet')).map(t => t.querySelector('.tweet-text').textContent);
 }
 
-// /**
-//  * Makes an REST call to and returns the result from the call
-//  *
-//  * @param
-//  * @returns
-//  */
-// const loadPage = async() => {
+/**
+ * Makes an REST call to and returns the result from the call
+ *
+ * @param
+ * @returns
+ */
+const loadPage = async() => {
   
-//   //tweets =  [ "@j_kloiber Hey my love, do you reckon we could hold a workshop on the 5th July too?","Bitch suck dick", "Hi! I’m unfortunately not around that day... sorry! (Let me see if I can figure something out with the keys..)", "Hey my love, any news on the 5th?", "@j_kloiber Hey my love, do you reckon we could hold a workshop on the 5th July too?", "Hi! I’m unfortunately not around that day... sorry! (Let me see if I can figure something out with the keys..)", "did i mention you are the best?!", "@j_kloiber Hey my love, do you reckon we could hold a workshop on the 5th July too?", "Hi! I’m unfortunately not around that day... sorry! (Let me see if I can figure something out with the keys..)"]
   
-//   tweets = getTweets()
+  tweets = getTweets();
 
-//   for (i = 0; i< tweets.length; i++) { 
-//   console.log(tweets[i])  
+  //tweets = [tweets[0]];
+
+  for (i = 0; i< tweets.length; i++) { 
+    console.log("We are inside LOAD PAGE")
+    response = browser.runtime.sendMessage({"tweetText":tweets[i]})
+    console.log("foo")
+    console.log(response)
+    const score = await response;
+
+ // tweet[i] = modifyHatefulTweet(score['score'], tweets[i])
+
+  }
+  console.log(tweets[1])
+
+  console.log("NOW HERE")
   
-//   var resp = await fetch("https://127.0.0.1:5000/predict", {
-//     method: 'post',
-//     body: JSON.stringify({ "sentences": tweets[i] }),
-//     headers:{
-//       'Content-Type': 'application/json',
-//     }
-//   }) 
+}
 
-//   const score = await resp.json();
 
-//   tweet[i] = modifyHatefulTweet(score['score'], tweets[i])
+/**
+ * Modifies the DOM when the tweet is deemed hateful or not
+ *
+ * @param
+ * @returns
+ */
+function modifyHatefulTweet(score, tweet) {
 
-//   }
-//   console.log(tweets[1])
+  if (score === 1) { tweet = "HARASSING";   console.log("in modify"); console.log(tweet); return tweet;}
 
-//   console.log("NOW HERE")
-  
-// }
-
-// /**
-//  * Modifies the DOM when the tweet is deemed hateful or not
-//  *
-//  * @param
-//  * @returns
-//  */
-// function modifyHatefulTweet(score, tweet) {
-
-//   if (score === 1) { tweet = "HARASSING";   console.log("in modify"); console.log(tweet); return tweet;}
-
-// }
+}
 
 console.log("HERE I AM CONTENT")
-debugger
-getTweets()
+loadPage()
 //document.addEventListener("DOMContentLoaded", loadPage)
 
 // module.exports = {
